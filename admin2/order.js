299579c11b09b7956265235d6dcbc8615e2bb32e
Kuldeep <kuldeepahir178@gmail.com>
2022-12-28 11:22:20 +0530

    first comit

---
+++ admin2/order.js
@@ -0,0 +1,50 @@
+const Orders = [
+    {
+    productName: 'jeans', 
+     productNumber: '85631',
+     paymentStatus: 'Due', 
+     shipping: 'Pending'
+    }, 
+    {
+    productName: 'Black Shirt' ,
+     productNumber: '36378',
+     paymentStatus: 'Refunded',
+      shipping: 'Declined'
+    }, 
+    {
+    productName: 'Jacket' ,
+    productNumber: '49347', 
+    paymentStatus: 'Due', 
+    shipping: 'Pending'
+    }, 
+    {
+        productName: 'Formal', 
+        productNumber: '96996', 
+        paymentStatus: 'Paid', 
+        shipping: 'Delivered',
+    },
+    {
+    
+    productName: 'Suit', 
+    productNumber: '22821', 
+    paymentStatus: 'Paid',
+     shipping: 'Delivered'
+    },
+
+    {
+    
+        productName: 'Coat', 
+        productNumber: '22821', 
+        paymentStatus: 'Paid',
+         shipping: 'Delivered'
+        },
+
+     {
+    
+            productName: 'Blazzer', 
+            productNumber: '22821', 
+            paymentStatus: 'Paid',
+             shipping: 'Delivered'
+            },
+    
+]
